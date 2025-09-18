03927f8e60fc646cbd7c5095fb43ff0580796795
Sandeep kumar <munnakumar74975@gmail.com>
2025-09-18 10:16:39 +0530

    update contact give location link

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -52,7 +52,7 @@ const Contact = () => {
               <FaMapMarkerAlt className='w-6 h-6 text-primary' />
               <div>
                 <h3 className='text-semibold'>Location</h3>
-                <p className='text-secondary hover:text-primary'>Lucknow Utterpardesh India</p>
+                <Link href='https://www.google.com/maps/place/Lucknow,+Uttar+Pradesh/@26.8486681,80.8601118,12z/data=!4m15!1m8!3m7!1s0x399bfd991f32b16b:0x93ccba8909978be7!2sLucknow,+Uttar+Pradesh!3b1!8m2!3d26.8466937!4d80.946166!16zL20vMDIydHE0!3m5!1s0x399bfd991f32b16b:0x93ccba8909978be7!8m2!3d26.8466937!4d80.946166!16zL20vMDIydHE0?authuser=0&entry=ttu&g_ep=EgoyMDI1MDkxNS4wIKXMDSoASAFQAw%3D%3D' className='text-secondary hover:text-primary'>Lucknow Utterpardesh India</Link>
               </div>
             </div>
           </div>
@@ -67,25 +67,21 @@ const Contact = () => {
               type="text" id='name' name='name' placeholder='Enter your Name'
               className='w-full px-4 py-2 rounded-md placeholder:text-white/50 border border-gray-300 dark:border-gray-700 bg-white dark:bg-dark focus:ring-2 focus:ring-primary focus:bordr-non' />
 
-            <div>
-              <form className='space-y-6'>
-                <label htmlFor="email" className='block text-sm font-medium mb-2'>Email</label>
-                <input
-                  required
-                  type="email" id='email' name='email' placeholder='Enter your Email'
-                  className='w-full px-4 py-2 rounded-md placeholder:text-white/50 border border-gray-300 dark:border-gray-700 bg-white dark:bg-dark focus:ring-2 focus:ring-primary focus:bordr-non' />
-              </form>
+            <div className='space-y-6'>
+              <label htmlFor="email" className='block text-sm font-medium mb-2'>Email</label>
+              <input
+                required
+                type="email" id='email' name='email' placeholder='Enter your Email'
+                className='w-full px-4 py-2 rounded-md placeholder:text-white/50 border border-gray-300 dark:border-gray-700 bg-white dark:bg-dark focus:ring-2 focus:ring-primary focus:bordr-non' />
             </div>
 
-            <div >
-              <form className='space-y-6'>
-                <label htmlFor="" className='block text-sm font-medium mb-2'>Message</label>
-                <textarea
-                  rows={4}
-                  required
-                  id='message' name='message' placeholder='Enter your Message'
-                  className='w-full px-4 py-2 rounded-md  placeholder:text-white/50 border border-gray-300 dark:border-gray-700 bg-white dark:bg-dark focus:ring-2 focus:ring-primary focus:bordr-non' />
-              </form>
+            <div className='space-y-6'>
+              <label htmlFor="message" className='block text-sm font-medium mb-2'>Message</label>
+              <textarea
+                rows={4}
+                required
+                id='message' name='message' placeholder='Enter your Message'
+                className='w-full px-4 py-2 rounded-md  placeholder:text-white/50 border border-gray-300 dark:border-gray-700 bg-white dark:bg-dark focus:ring-2 focus:ring-primary focus:bordr-non' />
             </div>
 
             <button type="submit" className='w-full btn btn-primary'>
